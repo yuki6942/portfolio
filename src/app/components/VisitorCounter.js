bcce0e01806f286ffaea6e151896a05ad62e543d
yuki6942 <56565042+yuki6942@users.noreply.github.com>
2025-05-25 15:48:08 +0200

    changed counter design

--- src/app/components/VisitorCounter.js
+++ src/app/components/VisitorCounter.js
@@ -3,7 +3,7 @@ export default function VisitorCounter() {
     <div className="mt-10">
       {/* fly-in-up */}
       <img
-        src="https://count.getloli.com/@6812rfv123obre80qwe79fftgawefbp034z82aqfw4e682gvz4qbg?name=6812rfv123obre80qwe79fftgawefbp034z82aqfw4e682gvz4qbg&theme=moebooru&padding=7&offset=0&align=top&scale=1&pixelated=1&darkmode=auto"
+        src="https://count.getloli.com/@6812rfv123obre80qwe79fftgawefbp034z82aqfw4e682gvz4qbg?name=6812rfv123obre80qwe79fftgawefbp034z82aqfw4e682gvz4qbg&theme=love-and-deepspace&padding=7&offset=0&align=top&scale=1&pixelated=1&darkmode=auto"
         alt="visitor counter"
       />
     </div>
